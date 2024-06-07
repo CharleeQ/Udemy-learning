401091cda82d146362b5637e92e19ec71c310c52
Kiryl Kakareka <charleequa@gmail.com>
2024-06-07 18:50:42 +0200

    init proj

---
+++ UdemyApp/UdemyApp/UdemyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyAppApp.swift
+//  UdemyApp
+//
+//  Created by Kiryl Kakareka on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct UdemyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
